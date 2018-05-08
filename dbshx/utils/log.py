eb947a62f1bf83f04248d4198ea0d936773fd59b
Dani Fdez <danifdezalvarez@gmail.com>
2018-05-08 12:52:10 +0200

    Close #6

---
+++ dbshx/utils/log.py
@@ -0,0 +1,7 @@
+from __future__ import print_function
+import sys
+
+
+def log_to_error(msg, source=None, target_log=sys.stderr):
+
+    print(msg + ". Source: " + (source if source is not None else "Not specified"), file=target_log)
