46d14cacdfb873c03ee76e121161adf0d811e899
Dani Fdez <danifdezalvarez@gmail.com>
2023-02-02 18:34:24 +0100

    Close #127 and #129

--- setup.py
+++ setup.py
@@ -21,6 +21,8 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+		"Programming Language :: Python :: 3.10",
+		"Programming Language :: Python :: 3.11",
         "Intended Audience :: Science/Research",
         "Intended Audience :: Information Technology",
         "Topic :: Software Development :: Libraries :: Python Modules"
