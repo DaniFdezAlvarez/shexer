65dc004dc60d18634e571f7746e6240350cfdbdc
Dani Fdez <danifdezalvarez@gmail.com>
2023-02-03 20:04:05 +0100

    Close #130 and close, probably #121

--- test/test_disable_or_statements.py
+++ test/test_disable_or_statements.py
@@ -9,7 +9,7 @@ _BASE_DIR = BASE_FILES + "disable_or" + pth.sep
 
 class TestGraphFileInput(unittest.TestCase):
 
-    def test_or_enabled(self):
+    def test_or_enabled_choice_useful_IRI(self):
         shaper = Shaper(graph_file_input=_BASE_DIR + "g3_or_example.ttl",
                         namespaces_dict=default_namespaces(),
                         all_classes_mode=True,
@@ -17,6 +17,20 @@ class TestGraphFileInput(unittest.TestCase):
                         disable_comments=True,
                         disable_or_statements=False)
         str_result = shaper.shex_graph(string_output=True)
+        # In case the choice includes the IRI macro, then no OR should appear
+        self.assertTrue(file_vs_str_tunned_comparison(file_path=_BASE_DIR + "or_disabled.shex",
+                                                      str_target=str_result,
+                                                      or_shapes=True))
+
+    def test_or_enabled_choice_expendable_IRI(self):
+        shaper = Shaper(graph_file_input=_BASE_DIR + "g_or_example_expandable_IRI.ttl",
+                        namespaces_dict=default_namespaces(),
+                        all_classes_mode=True,
+                        input_format=TURTLE,
+                        disable_comments=True,
+                        disable_or_statements=False)
+        str_result = shaper.shex_graph(string_output=True)
+        # In case the choice includes the IRI macro, then no OR should appear
         self.assertTrue(file_vs_str_tunned_comparison(file_path=_BASE_DIR + "or_enabled.shex",
                                                       str_target=str_result,
                                                       or_shapes=True))
