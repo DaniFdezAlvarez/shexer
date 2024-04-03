b45e57d88c96a62cf49ba8790f311f2676227634
DaniFdezAlvarez <danifdezalvarez@gmail.com>
2024-04-03 13:48:42 +0200

    Close #155

--- test/test_compression_mode.py
+++ test/test_compression_mode.py
@@ -13,6 +13,8 @@ _BASE_DIR = BASE_FILES + "compression" + pth.sep
 
 class TestCompressionMode(unittest.TestCase):
 
+    ######################### GZ
+
     def test_ttl_iter_gz(self):
         shaper = Shaper(
             graph_file_input=_BASE_DIR + "t_graph_1.ttl.gz",
@@ -234,3 +236,35 @@ class TestCompressionMode(unittest.TestCase):
             pass
         except:
             self.fail("The exception should be a ValueError")
+
+
+    ##################### SEVERAL FILES IN ZIP
+
+    def test_nt_zip(self):
+        shaper = Shaper(
+            graph_file_input=_BASE_DIR + "t1_graph_partials_1_2_3__3.nt.zip",
+            namespaces_dict=default_namespaces(),
+            all_classes_mode=True,
+            input_format=NT,
+            disable_comments=True,
+            compression_mode=ZIP
+        )
+        str_result = shaper.shex_graph(string_output=True)
+        self.assertTrue(file_vs_str_tunned_comparison(file_path=G1_ALL_CLASSES_NO_COMMENTS,
+                                                      str_target=str_result))
+
+    ##################### SEVERAL ZIPS
+
+    def test_nt_zip(self):
+        shaper = Shaper(
+            graph_list_of_files_input=[_BASE_DIR + "t1_graph_partials_1_2__3.nt.zip",
+                                       _BASE_DIR + "t1_graph_partials_3__3.nt.zip"],
+            namespaces_dict=default_namespaces(),
+            all_classes_mode=True,
+            input_format=NT,
+            disable_comments=True,
+            compression_mode=ZIP
+        )
+        str_result = shaper.shex_graph(string_output=True)
+        self.assertTrue(file_vs_str_tunned_comparison(file_path=G1_ALL_CLASSES_NO_COMMENTS,
+                                                      str_target=str_result))
